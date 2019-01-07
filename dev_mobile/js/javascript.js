5beca2c2e81951251570cfcff14054f2b5aa51bb
X-Track <45422444+X-Track@users.noreply.github.com>
2019-01-07 19:13:19 +0100

    v5

---
+++ dev_mobile/js/javascript.js
@@ -0,0 +1,20 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+if (w<950){
+document.getElementById("filters").style.display = "none";
+}
+
+function showFilters() {
+     var x = document.getElementById("filters");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+    }
+document.getElementById("filtersShowBtn").addEventListener("click", showFilters);
+
