5ec9be0d341c42a862833d51406479d5c4de2c9c
X-Track <xxttrack@gmail.com>
2019-01-12 15:27:19 +0100

    v10

--- js/javascript.js
+++ js/javascript.js
@@ -6,29 +6,38 @@
 
 /*      ALWAYS SHOW FILTERS DESKTOP      */
 
+
+
+var sPath = window.location.pathname;
+var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
 var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
-if (w < 950) {
-    document.getElementById("filters").style.display = "none";
-}
 
 
-/*      SHOW AND HIDE FILTERS      */
+if (sPage == "index.php") {
 
-function showFilters() {
-    var getFilters = document.getElementById("filters");
-    if (getFilters.style.display === "block") {
-        getFilters.style.display = "none";
+    if (w < 950) {
+        document.getElementById("filters").style.display = "none";
     } else {
-        getFilters.style.display = "block";
+
+
+        /*      SHOW AND HIDE FILTERS      */
+        function showFilters() {
+            var getFilters = document.getElementById("filters");
+            if (getFilters.style.display === "block") {
+                getFilters.style.display = "none";
+            } else {
+                getFilters.style.display = "block";
+            }
+        }
+
+        document.getElementById("filtersShowBtn").addEventListener("click", showFilters);
     }
+
 }
 
-document.getElementById("filtersShowBtn").addEventListener("click", showFilters);
 
-
-
-/*      MICRO INTERACTIE RATING      */
+/*      MICRO INTERACTIE COMMENTS      */
 
 var iconSelector = document.querySelector("section div article div span");
 var yeet = 0;
@@ -52,8 +61,86 @@ iconSelector.addEventListener("click", classChanger);
 
 var verrasMe = document.getElementById("verrasMe");
 
-function verrasMeFunc(){
-        window.location='ericLucassenv2.html';
-    }
+function verrasMeFunc() {
+    window.location = 'ericLucassenv2.html';
+}
 
 verrasMe.addEventListener("click", verrasMeFunc);
+
+
+/*      RAITING MICROINTERACTIE     */
+
+var star1 = document.getElementById("star1");
+var star2 = document.getElementById("star2");
+var star3 = document.getElementById("star3");
+var star4 = document.getElementById("star4");
+var star5 = document.getElementById("star5");
+var imgSource = document.getElementById("animatie");
+var yote = 1;
+var interval;
+
+
+/*   HIDE FEEDBACK  */
+function hideFeedback() {
+
+    imgSource.src = "images/10-desktop.svg";
+    imgSource.style.display = "none";
+    clearInterval(interval);
+}
+
+/*   SHOW FEEDBACK  */
+function showFeedback() {
+
+
+    imgSource.src = "images/ezgif.com-gif-maker.gif";
+    imgSource.style.display = "block";
+    console.log("yeet");
+
+
+    if (yote == 1) {
+
+        star1.classList = "fa fa-star starliked";
+        star2.classList = "fa fa-star starliked";
+        star3.classList = "fa fa-star starliked";
+
+        yote = 0;
+
+    } else {
+
+        star1.classList = "fa fa-star star";
+        star2.classList = "fa fa-star star";X
+        star3.classList = "fa fa-star star";
+
+        yote = 1;
+    }
+    interval = setInterval(hideFeedback, 1700);
+}
+
+/*   HIDE ERROR  */
+function hideError() {
+
+    imgSource.src = "images/10-desktop.svg";
+    imgSource.style.display = "none";
+    clearInterval(interval);
+}
+
+/*   SHOW ERROR  */
+function showError() {
+
+    imgSource.src = "images/ERROR.gif";
+    imgSource.style.display = "block";
+
+    console.log("yeet");
+
+    star1.classList = "fa fa-star star";
+    star2.classList = "fa fa-star star";
+    star3.classList = "fa fa-star star";
+
+    interval = setInterval(hideError, 1700);
+}
+
+star1.addEventListener("click", showFeedback);
+star2.addEventListener("click", showFeedback);
+star3.addEventListener("click", showFeedback);
+star4.addEventListener("click", showError);
+star5.addEventListener("click", showError);
--- js/javascriptComments.js
+++ js/javascriptComments.js
@@ -2,33 +2,152 @@
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
 
-var star1 = document.getElementById("star1");
-var star2 = document.getElementById("star2");
-var star3 = document.getElementById("star3");
-var star4 = document.getElementById("star4");
-var star5 = document.getElementById("star5");
 
-var imgSource = document.getElementById("animatie");
+var yote;
+var findClass;
+var getStarsForChangeClass = document.querySelectorAll(".yeet");
 
-function hideAnimation() {
+var imageFolder = "images/";
+
+var imgSource = document.querySelector(".ratingsComments div:last-of-type img");
+
+var interval;
+
+var getStars1;
+var getSpan1 = document.querySelectorAll(".yeet");
+var getStars2;
+var getSpan2 = document.querySelectorAll(".yote");
 
 
-    imgSource.src = "images/10-desktop.svg";
+
+
+function classChangerStars() {
+    if (yote === 1) {
+
+        getStarsForChangeClass.forEach(findClass => {
+            findClass.classList.remove("star");
+            findClass.classList.add("starliked");
+        });
+    } else if (yote === 0) {
+
+        getStarsForChangeClass.forEach(findClass => {
+            findClass.classList.remove("starliked");
+            findClass.classList.add("star");
+        });
+    }
+}
+
+
+
+
+function hideFeedback() {
+    imgSource.src = imageFolder + "10-desktop.svg";
     imgSource.style.display = "none";
+
+    clearInterval(interval);
 }
 
-function showAnimation() {
+function showFeedback() {
 
-    imgSource.src = "images/ezgif.com-gif-maker.gif";
+    imgSource.src = imageFolder + "ezgif.com-gif-maker.gif";
     imgSource.style.display = "block";
-    console.log("yeet");
-    setInterval(hideAnimation, 2000);
+    yote = 1;
+    classChangerStars();
+
+    interval = setInterval(hideFeedback, 1700);
 }
 
-star1.addEventListener("click", showAnimation);
-star2.addEventListener("click", showAnimation);
-star3.addEventListener("click", showAnimation);
-star4.addEventListener("click", showAnimation);
-star5.addEventListener("click", showAnimation);
+getSpan1.forEach(getStars1 => {
+
+    getStars1.addEventListener("click", showFeedback);
+});
+
+
+
+
+function hideError() {
+
+    imgSource.src = imageFolder + "10-desktop.svg";
+    imgSource.style.display = "none";
+    clearInterval(interval);
+}
+
+function showError() {
+
+    imgSource.src = imageFolder + "ERROR.gif";
+    imgSource.style.display = "block";
+    yote = 0;
+    classChangerStars();
+
+    interval = setInterval(hideError, 1700);
+}
+
+getSpan2.forEach(getStars2 => {
+
+    getStars2.addEventListener("click", showError);
+});
+
+
+
+/*          Text Counter          */
+/*          https://stackoverflow.com/questions/12742595/show-how-many-characters-remaining-in-a-html-text-box-using-javascript/12742721
+ */
+
+function textCounter(field, field2, maxlimit) {
+    var countfield = document.getElementById(field2);
+    if (field.value.length > maxlimit) {
+        field.value = field.value.substring(0, maxlimit);
+        return false;
+    } else {
+        countfield.value = maxlimit - field.value.length;
+    }
+}
+
+
+
+/*          REVIEWS          */
+
+var article = document.querySelector(".ratingsComments section article:first-of-type");
+
+
+
+/*          REVIEW PLAATSEN          */
+
+var plaatsReviewButton = document.querySelector(".ratingsComments section button");
+
+function classChangerShowArticle() {
+
+    document.getElementById("message").value = "";
+    document.getElementById("counter").value = "135";
+    article.classList.remove("hide");
+    article.classList.add("show");
+
+}
+
+plaatsReviewButton.addEventListener("click", classChangerShowArticle);
+
+
+
+
+/*          REVIEW VERWIJDEREN          */
+
+var verwijderButton = document.querySelector(".ratingsComments section article h6");
+
+function classChangerHideArticle() {
+
+    article.classList.remove("show");
+    article.classList.add("hide");
+
+}
+
+verwijderButton.addEventListener("click", classChangerHideArticle);
+
+
+
+
+
+
+
+
 
 
